b18e57046c399319bca6bfc6acd10762c17fd38c
Nikolai Voronin <Mitay89@bk.ru>
2022-07-09 23:10:59 +0300

    Add todo.go, user.go which contains some structs

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/IDontCareMe/todo-app"
+)
+
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
