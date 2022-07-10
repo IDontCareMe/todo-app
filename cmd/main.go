fc934a923cc4d7c16451c4c44ed5dfc6e227d0e0
Nikolai Voronin <Mitay89@bk.ru>
2022-07-10 14:43:27 +0300

    creating repo on github... testing...

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 
+	"github.com/IDontCareMe/todo-app/pkg/handler"
+
 	"github.com/IDontCareMe/todo-app"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
