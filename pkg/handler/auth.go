35cf211a645f6faac07437703b22a6171fc8e3ae
Nikolai Voronin <Mitay89@bk.ru>
2022-07-10 22:28:27 +0300

    service.go and repository.go has added

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -4,10 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func (h *Handler) signUp(c gin.Context) {
+func (h *Handler) signUp(c *gin.Context) {
 
 }
 
-func (h *Handler) signIn(c gin.Context) {
+func (h *Handler) signIn(c *gin.Context) {
 
 }
